2908cc86b9d2721956fa4d3269e92c01b223078a
tdhuynh <tommyhuynh93@gmail.com>
2016-12-21 15:19:04 -0500

    added custom domain to settings

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tdh-portfolio.herokuapp.com', 'localhost', 'tommyhuynh.io']
+ALLOWED_HOSTS = ['tdh-portfolio.herokuapp.com', 'localhost', 'www.tommyhuynh.io', 'tommyhuynh.io']
 
 
 # Application definition
