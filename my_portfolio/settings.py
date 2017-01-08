7ea7657116164cfaa0a533c938a9d3529654b78a
tdhuynh <tommyhuynh93@gmail.com>
2017-01-08 18:35:40 -0500

    changed aws custom domain

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -133,7 +133,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
 
 if AWS_STORAGE_BUCKET_NAME:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
     AWS_S3_FILE_OVERWRITE = False
     STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
